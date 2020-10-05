8af2e32395e7c2242d91bee6e3ffec76d6e27294
McBride340 <69330880+McBride340@users.noreply.github.com>
2020-10-04 21:25:22 -0400

    Update CalculatorApp.cs

--- Calculator/CalculatorApp.cs
+++ Calculator/CalculatorApp.cs
@@ -11,6 +11,21 @@ namespace Calculator
         {
             return a + b;
         }
+        
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
 
 
     }
